ec8c2c61620042f376e1a5b5cf62d0362f6e7a80
rapioanfernee <rapioanfernee@gmail.com>
2020-08-28 19:22:46 +0800

    Updates

---
+++ precache-manifest.7d598d768dc371aaa207fc2f936018b0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2658a6f5d5fe80cadc9ae788e18b0f",
+    "url": "/portfolio-update/index.html"
+  },
+  {
+    "revision": "095ccb39593a79a3dfa2",
+    "url": "/portfolio-update/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4cced1e716fa0276fd29",
+    "url": "/portfolio-update/static/css/main.636acef0.chunk.css"
+  },
+  {
+    "revision": "095ccb39593a79a3dfa2",
+    "url": "/portfolio-update/static/js/2.8ed74290.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-update/static/js/2.8ed74290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cced1e716fa0276fd29",
+    "url": "/portfolio-update/static/js/main.8e60ca4f.chunk.js"
+  },
+  {
+    "revision": "abae65e19ce793c06ce6",
+    "url": "/portfolio-update/static/js/runtime-main.a9ec9ad0.js"
+  },
+  {
+    "revision": "f8bb717dcd05acb82797e6c47c832a8d",
+    "url": "/portfolio-update/static/media/app-background.f8bb717d.jpeg"
+  },
+  {
+    "revision": "d19b6213293eacbc8b22ab2c636c6c2b",
+    "url": "/portfolio-update/static/media/innovationlove.d19b6213.png"
+  },
+  {
+    "revision": "6dd159cf029352bcca27368ff66eb2e4",
+    "url": "/portfolio-update/static/media/onepropertee.6dd159cf.png"
+  }
+]);
